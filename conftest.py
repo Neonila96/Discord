f6ddbdc66d34012e9a2977c69d7781663f5b8d4f
Neonila96 <neonila.zinova@icloud.com>
2024-10-26 15:50:54 +0500

    Update conftest(headless).py

--- conftest.py
+++ conftest.py
@@ -10,11 +10,11 @@ from login_page import LoginPage
 def init_driver(request):
     # Инициализация драйвера
 
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
-    # driver = webdriver.Chrome(options=options)
+     options = webdriver.ChromeOptions()
+     options.add_argument('--headless')
+     driver = webdriver.Chrome(options=options)
 
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     driver.maximize_window()
     request.cls.driver = driver
